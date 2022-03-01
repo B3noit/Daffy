287b3914b2db04846e696d4546d792cd8b190e05
BenLama <b3noit.lam@hotmail.fr>
2022-03-01 20:41:14 +0100

    transition to c++ to follow gameDevTV tutorials

---
+++ FrenchCanardMetaVers/Source/FrenchCanardMetaVers/FrenchCanardMetaVers.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FrenchCanardMetaVers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FrenchCanardMetaVers, "FrenchCanardMetaVers" );
---
+++ FrenchCanardMetaVers/Source/FrenchCanardMetaVers/WidgetMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetBlueprint.h"
+#include "WidgetMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRENCHCANARDMETAVERS_API UWidgetMenu : public UWidgetBlueprint
+{
+	GENERATED_BODY()
+	
+};
